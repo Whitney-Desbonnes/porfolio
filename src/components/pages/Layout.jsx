3901ee90a83833b1bdd0b095fcd645ba3c6ff9e2
Whitney <whitney.desbonnes@outlook.fr>
2024-11-26 17:39:22 +0100

    ✨ 01 - Layout.jsx > Creation layout page, adding dark and light mode, adding outlet

---
+++ src/components/pages/Layout.jsx
@@ -0,0 +1,95 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import styled from "styled-components";
+import { IoMoonOutline } from "react-icons/io5";
+import { MdOutlineWbSunny } from "react-icons/md";
+import { useEffect, useState } from "react";
+
+export default function Layout() {
+    // state (état, données)
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", isDarkMode ? "dark" : "light");
+      }, [isDarkMode]);
+    
+    // comportements
+    const toggleTheme = () => setIsDarkMode(!isDarkMode);
+
+    // render / rendu
+    return(
+        <>
+            <IconThemeStyled>
+                {isDarkMode ? 
+                    (
+                        <>
+                            <MdOutlineWbSunny onClick={toggleTheme} />
+                            <span>Mode sombre activé</span>
+                        </>
+
+                    ) 
+                    : 
+                    (
+                        <>
+                            <IoMoonOutline onClick={toggleTheme} /> 
+                            <span>Mode clair activé</span>
+                        </>
+                    )
+                }
+            </IconThemeStyled>
+
+
+            <Navbar/>
+
+            <MainStyled>
+                <div>
+                    Côté gauche
+                </div>
+
+                <div>
+                    <Outlet/>
+                </div>
+            </MainStyled>
+        </>
+    )
+}
+
+const MainStyled = styled.div`
+    display: flex;
+    gap: 20px;
+
+    > div:first-child {
+        width: 30%; 
+        background-color: var(--bgc-card);
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    > div:last-child {
+        width: 70%; 
+        background-color: var(--bgc-card);
+        border-radius: 10px;
+        padding: 20px;
+    }
+`
+
+const IconThemeStyled = styled.span`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 30px;
+
+    svg {
+        color: var(--theme-color-icon);
+        cursor: pointer;
+        font-size: 24px;
+        padding: 10px;
+        background-color: var(--bgc-links);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 100px;
+        height: 50px;
+        width: 50px;
+    }
+`
